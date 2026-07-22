9b596171f0c6015456259a550740c8f2a1e52dc7
Swetha P <swetha29102004@gmail.com>
2026-07-22 14:38:20 +0530

    Add files via upload

---
+++ logical.py
@@ -0,0 +1,35 @@
+# prime number 
+
+# num = int(input("enter a number"))
+# if num > 1:
+#     for i in range(2,num):
+#         if num % i ==0:
+#             print("Not prime")
+#             break
+#     else:
+#         print("prime number")
+# else:
+#     print("Not a prime number")
+
+#reverse a list 
+# nums =[10,20,30,40,50]
+# a = reversed(nums)
+# for i in a:
+#     print(a)
+# result = list(a)
+
+# n = 7
+# for i in range(1, n +1):
+#     for j in range(n-i):
+#          print(" ",end="")
+#     for k in range(2 * i -1):
+#          print("*", end ="")
+#     print()
+# for i in range(n-1 ,0, -1): 
+#     for j in range(n - i):  
+#         print(" ",end ="")
+#     for k in range(2 * i -1):  
+#         print("*", end ="")
+#     print()
+
+
